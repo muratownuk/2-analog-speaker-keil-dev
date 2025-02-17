32749646b533a06ea62c0011809d0d41210da045
Murat Ownuk <ilmuratownuk@gmail.com>
2025-02-17 10:50:23 -0500

    added skeleton/template code for analog speaker files

--- analog_speaker.c
+++ analog_speaker.c
@@ -1,1 +1,90 @@
+/*
+    digital_speaker: 
+    this module contains anything related to the speaker side of the project. 
+*/
+#define _DIGITAL_SPEAKER_C_ 
+
+/*
+    includes 
+*/
+#include "./SiLABS/c8051F000.h" 
+#include "digital_speaker.h" 
+
+/*
+    routine prototypes 
+*/
+void vTimer1_ISR(void); 
+static void vFrequencyChange(void);
+
+/*
+    sbit defintions 
+*/
+sbit            SPEAKER     = P1^0; 
+
+/*
+    global variables 
+*/
+unsigned char   count       = COUNT_MIN; 
+unsigned char   count_lead  = COUNT_MIN+1; 
+bit             INC_DEC     = INC; 
+unsigned int    count_ISR   = ISR_COUNT;        // 500  
+
+/*
+    routines 
+*/
+/*
+    vTimer1_ISR: 
+    services the timer 1 interrupt, which in this case, toggle SPEAKER bit 
+    after 100us*count. count_lead equality defines frequency of toggle and is 
+    directly changed with vFrequencyChange routine.   
+*/
+void vTimer1_ISR(void) interrupt TIMER1_INT{ 
+
+    if(!(--count_ISR)){                 // 100us*ISR_COUNT=50ms 
+        count_ISR=ISR_COUNT;            // reset ISR run counter 
+        vFrequencyChange();             // change freq after above duration
+    }                   
+
+    count++;                            // 100us increment
+
+    if (count==count_lead){
+        SPEAKER=~SPEAKER;               // toggle speaker (on/off)
+        count=COUNT_MIN;                // reset 100us increment counter
+    }
+
+}
+
+/*
+    vFrequencyChange: 
+    changes count_lead global variable, effectively increasing/decreasing 
+    frequency of the tone. this will produce siren sound effect.  
+
+*/
+static void vFrequencyChange(void){ 
+
+    if(INC_DEC){ 
+        count_lead++;                   // effectively decreases frequency 
+
+        // go into decrement
+        if(count_lead==COUNT_MAX){ 
+            count_lead--;
+            INC_DEC=DEC; 
+        }
+
+    }
+    else{ 
+        count_lead--;                   // effectively increases frequency 
+
+        // go into increment 
+        if(count_lead==COUNT_MIN){ 
+            count_lead++; 
+            INC_DEC=INC; 
+        }
+
+    }
+
+} 
+
+
+
  
--- analog_speaker.h
+++ analog_speaker.h
@@ -1,1 +1,25 @@
+#ifndef _DIGITAL_SPEAKER_H_ 
+    #define _DIGITAL_SPEAKER_H_ 
+
+    /*
+        defines 
+    */ 
+    #define     TIMER1_INT  3 
+    #define     DEC         0 
+    #define     INC         1 
+    #define     COUNT_MAX   50 
+    #define     COUNT_MIN   0 
+    #define     ISR_COUNT   500 
+
+    #ifndef _DIGITAL_SPEAKER_C_
+        
+        // function prototypes (for external use) 
+        extern void vTimer1_ISR(void); 
+  
+    #endif
+
+#endif 
+
+
+
  
